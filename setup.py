0c8a263393fce7f2d4be4036a9ad6f58a475141b
Emilio Graff <1@emil.io>
2022-07-31 08:37:36 -0700

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Log',
+    version='1.0.0',
+    url='https://github.com/shaperilio/logging.git',
+    author='Emilio Graff',
+    author_email='1@emil.io',
+    description="Emilio's logging setup",
+    packages=find_packages(),
+    install_requires=['colorama'],
+)
